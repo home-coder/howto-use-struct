dff328ae736b212e25fc63bcf0ce12b7865a8618
home-coder <one_face@sina.com>
2017-05-16 22:48:22 +0800

    不使用行指针  同样很漂亮的完成了输出结构体中二维数组元素并过滤掉最后垃圾元素的 整个需求

--- yang.c
+++ yang.c
@@ -35,7 +35,7 @@ static void print_yang(struct yangst yznlife, int (*ppt)[10])
 	/*print parttime, 使用行指针会将结果处理的很漂亮*/
 	int ptrow = sizeof(yznlife.parttime)/sizeof(yznlife.parttime[0]);
 	int ptcol = sizeof(yznlife.parttime[0]);
-//	typedef int (*pparttime)[ptcol];
+	//	typedef int (*pparttime)[ptcol];
 	int j;
 	for (i = 0; i < ptrow; i++) {
 		for (j = 0; j < ptcol; j++) {
@@ -46,7 +46,45 @@ static void print_yang(struct yangst yznlife, int (*ppt)[10])
 		}
 	}
 
+	printf("\n-------------------------------------\n");
+}
+
+static void print_yang2(struct yangst yznlife)
+{
+	int bylen = sizeof(yznlife.big_year)/sizeof(yznlife.big_year[0]);
+	int pnlen = sizeof(yznlife.partners)/sizeof(yznlife.partners[0]);
+
+	printf("name = %s\n", yznlife.name);
+	printf("no   = %c\n", yznlife.no);
+	printf("age  = %d\n", yznlife.age);
+	printf("wife = %s\n", yznlife.wife);
+	printf("big_year = ");
+
+	/*print big year*/
+	int i = 0;
+
+	for (; i < bylen; i++) {
+		if (yznlife.big_year[i] == -0xff) {
+			break;
+		}
+		printf("%d,", yznlife.big_year[i]);
+	}
 	printf("\n");
+
+	/*print partner time*/
+	int ptrow = sizeof(yznlife.parttime)/sizeof(yznlife.parttime[0]);
+	int ptcol = sizeof(yznlife.parttime[0]);
+	int j;
+	for (i = 0; i < ptrow; i++) {
+		for (j = 0; j < ptcol; j++) {
+			if ( yznlife.parttime[i][j] == -0xff ) {
+				break;
+			}
+			printf("%d, ", yznlife.parttime[i][j]);
+		}
+	}
+
+	printf("\n-------------------------------------\n");
 }
 
 int main()
@@ -66,6 +104,20 @@ int main()
 		typedef int (*pptime)[10];
 		pptime ppt = yznlife.parttime;
 		print_yang(yznlife, ppt);
+	}
+//1.1 声明时初始化结构体，不包含赋值操作, 不使用行指针也是可以做到的，也很漂亮啊
+	{
+		struct yangst yznlife = {
+			.name = "Yangzhenning",
+			.no   = 'A',
+			.age  = 95,
+			.wife = "Duzhili",
+			.big_year = {1942, 1954, 1966, 1971, 2003, -0xff},
+			.partners = {"Mils", "Lizhengdao", "Wengfan", NULL},
+			.parttime = {{1956, 1957, 1958, -0xff}, {1963, 1964, -0xff}},
+		};
+
+		print_yang2(yznlife);
 	}
 
 	return 0;
