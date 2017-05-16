87f342ea2bde03673aad895699ef82f3497a571a
jiangxiujie <jiangxiujie@cantv.cn>
2017-05-16 18:30:02 +0800

    注意两点:1,结构体中数组必须是可知的，如parttime[][10]编译可以通过，但是初始化就不可以了；2.sizeof编译时知道大小，但是不能写成int a[sizeof(xxx)]

--- yang.c
+++ yang.c
@@ -7,20 +7,24 @@ struct yangst {
 	char wife[32];
 	int  big_year[30]; //1954
 	char *partners[10];
-	int  parttime[][10];//{{1956， 1957， 1958},{1963， 1964}， };
+	int  parttime[2][10];//{{1956， 1957， 1958},{1963， 1964}， };
 };
 
 static void print_yang(struct yangst yznlife)
 {
 	int bylen = sizeof(yznlife.big_year)/sizeof(yznlife.big_year[0]);
 	int pnlen = sizeof(yznlife.partners)/sizeof(yznlife.partners[0]);
+	int ptrow = sizeof(yznlife.parttime)/sizeof(yznlife.parttime[0]);
 
 	printf("name = %s\n", yznlife.name);
 	printf("no   = %c\n", yznlife.no);
 	printf("age  = %d\n", yznlife.age);
 	printf("wife = %s\n", yznlife.wife);
 	printf("big_year = ");
+
+	/*print big year*/
 	int i = 0;
+
 	for (; i < bylen; i++) {
 		if (yznlife.big_year[i] == -0xff) {
 			break;
@@ -29,11 +33,17 @@ static void print_yang(struct yangst yznlife)
 	}
 	printf("\n");
 
+	/*print parttime, 使用行指针会将结果处理的很漂亮*/
+	int ptcol = sizeof(yznlife.parttime[0]);
+	typedef int (*pparttime)[ptcol];
+	pparttime ppt = yznlife.parttime;
+
+	printf("\n");
 }
 
 int main()
 {
-//1.0 初始化结构体，不包含赋值操作
+//1.0 声明时初始化结构体，不包含赋值操作
 	{
 		struct yangst yznlife = {
 			.name = "Yangzhenning",
@@ -42,7 +52,7 @@ int main()
 			.wife = "Duzhili",
 			.big_year = {1942, 1954, 1966, 1971, 2003, -0xff},
 			.partners = {"Mils", "Lizhengdao", "Wengfan", NULL},
-			//.parttime = {{1956, 1957, 1958}, {1963, 1964}},
+			.parttime = {{1956, 1957, 1958, -0xff}, {1963, 1964, -0xff}},
 		};
 
 		print_yang(yznlife);
