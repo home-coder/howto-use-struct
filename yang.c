dd8e6feb5bbf7544328727069b93ffbd1680e397
jiangxiujie <jiangxiujie@cantv.cn>
2017-05-16 16:05:17 +0800

    gnu c 必须这样初始化，记住我不用ansi的c

--- yang.c
+++ yang.c
@@ -8,10 +8,10 @@ int main()
 			char *name;
 			char no;
 			int  age;
-			char wife[];
-			int  big_year[]; //1954
-			char *partners[];
-			int  parttime[][];//{{1956， 1957， 1958},{1963， 1964}， };
+			char wife[32];
+			int  big_year[30]; //1954
+			char *partners[10];
+			int  parttime[][10];//{{1956， 1957， 1958},{1963， 1964}， };
 		};
 
 	}
