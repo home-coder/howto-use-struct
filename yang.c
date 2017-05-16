08b6ec5dff0c83f9982d245ab3347c9d50d657aa
jiangxiujie <jiangxiujie@cantv.cn>
2017-05-16 17:08:26 +0800

    1.0, 结构体初始化，思考其中指针，数组，多维数组的初始化与不在结构体的不同

--- yang.c
+++ yang.c
@@ -1,19 +1,51 @@
 #include <stdio.h>
 
+struct yangst {
+	char *name;
+	char no;
+	int  age;
+	char wife[32];
+	int  big_year[30]; //1954
+	char *partners[10];
+	int  parttime[][10];//{{1956， 1957， 1958},{1963， 1964}， };
+};
+
+static void print_yang(struct yangst yznlife)
+{
+	int bylen = sizeof(yznlife.big_year)/sizeof(yznlife.big_year[0]);
+	int pnlen = sizeof(yznlife.partners)/sizeof(yznlife.partners[0]);
+
+	printf("name = %s\n", yznlife.name);
+	printf("no   = %c\n", yznlife.no);
+	printf("age  = %d\n", yznlife.age);
+	printf("wife = %s\n", yznlife.wife);
+	printf("big_year = ");
+	int i = 0;
+	for (; i < bylen; i++) {
+		if (yznlife.big_year[i] == -0xff) {
+			break;
+		}
+		printf("%d,", yznlife.big_year[i]);
+	}
+	printf("\n");
+
+}
+
 int main()
 {
-//1. struct 的声明定义
+//1.0 初始化结构体，不包含赋值操作
 	{
-		struct yangst {
-			char *name;
-			char no;
-			int  age;
-			char wife[32];
-			int  big_year[30]; //1954
-			char *partners[10];
-			int  parttime[][10];//{{1956， 1957， 1958},{1963， 1964}， };
+		struct yangst yznlife = {
+			.name = "Yangzhenning",
+			.no   = 'A',
+			.age  = 95,
+			.wife = "Duzhili",
+			.big_year = {1942, 1954, 1966, 1971, 2003, -0xff},
+			.partners = {"Mils", "Lizhengdao", "Wengfan", NULL},
+			//.parttime = {{1956, 1957, 1958}, {1963, 1964}},
 		};
 
+		print_yang(yznlife);
 	}
 
 	return 0;
